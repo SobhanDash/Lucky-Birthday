18caf50dba1724403ca6ecb6b1b29485767dc554
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2021-01-27 18:24:44 +0530

    style: added footer, policy card

--- app.js
+++ app.js
@@ -23,15 +23,25 @@ function isLucky(){
     }
     else if(numlist[0]==="" || numlist[1]==="" || numlist[2]===""){
         document.getElementById("output").style.display = "block";
-        outputDiv.innerText = "Enter The Date";
+        outputDiv.innerText = "Enter The Date Completely";
     }
     else if(sum % lucky === 0){
         document.getElementById("output").style.display = "block";
-        outputDiv.innerText = "Your Bithday is lucky!";
+        outputDiv.innerText = "Congratulations! Your Bithday is lucky! 💯🎉";
     }
     else{
         document.getElementById("output").style.display = "block";
-        outputDiv.innerText = "Your Bithday is not lucky :(";
+        outputDiv.innerText = "Sorry but Your Bithday is not lucky :(";
     }
     
 }
+
+// policy card
+const policy = document.querySelector("#policy");
+const policyCard = document.querySelector("#policy-card");
+
+policy.addEventListener('click', function() {
+    policyCard.className = "show";
+    setTimeout(function() { 
+        policyCard.className = policyCard.className.replace("show", ""); }, 3000);
+})
