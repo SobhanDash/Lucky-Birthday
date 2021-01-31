f57fd45ea40fa54f14b49a1cc624239f5633d092
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2021-01-31 19:41:40 +0530

    minor fix

--- app.js
+++ app.js
@@ -48,6 +48,5 @@ policy.addEventListener('click', function() {
 
 // force zoom out
 var scale = 'scale(0.9)';
-document.body.style.webkitTransform = scale; // Chrome, Opera, Safari
 document.body.style.msTransform = scale; // IE 9
 document.body.style.transform = scale; // General
